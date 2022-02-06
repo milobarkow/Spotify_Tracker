1144360ad7b1585265527e6a46c9c7e765104e0e
12158 <milohbarkow@gmail.com>
2022-02-06 15:18:26 -0500

    File name changes

--- playlist_csv.py
+++ playlist_csv.py
@@ -1,17 +1,18 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import os
+from spotipy.oauth2 import SpotifyOAuth, SpotifyPKCE
 import pandas as pd
 
-
-CLIENTID = '7a9404710e8b4fcea7f0e5634f000c96'
-CLIENTSECRET = '9b35c0b6042147e898aec07ff46a2e0b'
+CLIENTID = os.environ['CLIENTID']
 redirect_uri = 'http://localhost:8888/callback'
 scope = 'playlist-read-private user-library-read'
 
-sp = spotipy.Spotify(requests_timeout=50, auth_manager=SpotifyOAuth(client_id=CLIENTID,
-                                                                    client_secret=CLIENTSECRET,
-                                                                    redirect_uri=redirect_uri,
-                                                                    scope=scope))
+auth_PKCE = SpotifyPKCE(client_id=CLIENTID,
+                        redirect_uri=redirect_uri,
+                        scope=scope,
+                        open_browser=True)
+
+sp = spotipy.Spotify(requests_timeout=50, auth_manager=auth_PKCE)
 
 data_dict = sp.current_user_playlists(offset=0, limit=50)
 
@@ -28,6 +29,7 @@ while offset < 200:
         pid = playlist['id']
         body = sp.playlist(pid)
         name = body['name']
+        print(name)
         track_count = body['tracks']['total']
         song_list.append(track_count)
         of = 0
@@ -51,3 +53,7 @@ for key in final:
         final[key].append(' ')
 final_csv = pd.DataFrame(final).to_csv('final.csv', index=False)
 
+try:
+    os.remove('.cache')
+except OSError as e:
+    print("Error: %s : %s" % ('.cache', e.strerror))
